0154ce6c9d7e6a47949af011e26e8836e91ad5f1
Whooocash <119784@student.san.edu.pl>
2024-06-05 16:48:32 +0100

    help2

---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Todo } from "./Todo";
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { EditTodoForm } from "./EditTodoForm";
+
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  const deleteTodo = (id) => setTodos(todos.filter((todo) => todo.id !== id));
+
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  // Obliczanie procentu wykonanych zadań
+  const completedPercentage = (todos.filter(todo => todo.completed).length / todos.length) * 100;
+
+  return (
+    <div className="TodoWrapper">
+      <div className="info">
+        <p>Kliknij na zadanie, aby oznaczyć je jako zakończone.</p>
+        <p>Oliwia Łukasik nr.indeksu 119784</p>
+      </div>
+      <h1>Green Garden project JS</h1>
+      <div className="progress-bar-container">
+        <div className="progress-bar" style={{ width: `${completedPercentage}%` }}></div>
+      </div>
+      <TodoForm addTodo={addTodo} />
+      {todos.map((todo) =>
+        todo.isEditing ? (
+          <EditTodoForm editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            key={todo.id}
+            task={todo}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+            toggleComplete={toggleComplete}
+          />
+        )
+      )}
+    </div>
+  );
+};
